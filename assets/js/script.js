404bc24a78fc1c5d100921b9da4596b943e246af
Mersant <dlowry99@outlook.com>
2022-08-14 02:11:07 -0400

    Final commit
    Calendar now fully functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,63 @@
-var dailyEventsStorage = new Array(9);
+// dailyEventsStorage is effectively the same as what is in localStorage
+var dailyEventsStorage = new Array(9).fill("");
 if(!localStorage.dailyEventsStorage) {
     localStorage.setItem("dailyEventsStorage", JSON.stringify(dailyEventsStorage));
 }
+// Add all the daily events to the text boxes first so that updateDocument() can adjust the border colors of the time cards based off of this.
+var dailyEvents = JSON.parse(localStorage.dailyEventsStorage);
+for(i=0; i<= 8; i++) {
+    document.getElementById("textInput" + i).value = dailyEvents[i]
+}
 
+// Colorize the time cards based on whether or not they are in the past and/or have an event scheduled
 function updateDocument() {
     var now = moment();
     document.getElementById("currentTime").textContent = now.format('MMMM Do YYYY, h:mm:ss a');
-    console.log(now.format('H'));
-    // Check if a timecard already happened today. If so, set its background color to gray
+
     for(i=0; i<=8; i++) {
-        console.log(i);
-        if(document.getElementById("timeStamp" + i).textContent <= now.format('H')) {
+        // If time card is in the past
+        console.log(document.getElementById("timeStamp" +i).textContent)
+        console.log(now.format('H'))
+        if( document.getElementById("timeStamp" + i).textContent <= now.format('H') ) {
             document.getElementById("inputField" + i).style.backgroundColor = "gray";
+            // If this timecard already happened and had an event scheduled for it
+            if( document.getElementById("textInput" + i).value ) {
+                document.getElementById("inputField" + i).style.borderColor = "red";
+            } else {
+                document.getElementById("inputField" + i).style.borderColor = "black";
+            }
+        }
+        // If timecard is in the future and has something scheduled for it
+        else {
+            document.getElementById("inputField" + i).style.backgroundColor = "white";
+
+            if( document.getElementById("textInput" + i).value ) {
+                document.getElementById("inputField" + i).style.borderColor = "green";
+            }
+            else {
+                document.getElementById("inputField" + i).style.borderColor = "black";
+            }
         }
     }
 }
-updateDocument(); // Prevents user from having to wait 1 second to see the current time
+updateDocument(); // Prevents user from having to wait 1 second to see the current time and colors of the document
 setInterval(updateDocument, 1000);
 
-function updateEvents() {
+// Executed every time the user clicks on the save buttons. Each time slot is saved individually by each save button.
+function updateData(index) {
     var dailyEvents = JSON.parse(localStorage.dailyEventsStorage);
-    for(i=0; i<= 8; i++) {
-        document.getElementById("textInput" + i).value = dailyEvents[i]
-    }
-}
-updateEvents();
 
-// Executed every time the user clicks on the save buttons
-function updateData() {
-    var now = moment()
-    for(i=0; i<=8; i++) {
-        var temp =  document.getElementById("textInput" + i).value
-        dailyEventsStorage[i] = temp;
-        if(temp == "") {
-            document.getElementById("inputField" + i).style.backgroundColor = "white";
-        }
+    if( document.getElementById("textInput" + index).value ) {
+        dailyEventsStorage[index] = document.getElementById("textInput" + index).value;
+    } else {
+        dailyEventsStorage[index] = "";
     }
     localStorage.setItem("dailyEventsStorage", JSON.stringify(dailyEventsStorage));
-    document.getElementById("statusUpdate").textContent = "localStorage was updated at " + now.format('MMMM Do YYYY, h:mm:ss a');
+
+    var now = moment()
+    // Ex. Item 1 in localStorage was updated at August 14th 2022, 1:26:02 am with pet doggos
+    document.getElementById("statusUpdate").textContent = "Item " + (index+1) + " in localStorage was updated at " + now.format('MMMM Do YYYY, h:mm:ss a') + " with \"" + dailyEventsStorage[index] + "\"";
+    updateDocument();
 }
 
 
